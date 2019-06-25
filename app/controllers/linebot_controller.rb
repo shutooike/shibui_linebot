7080dff9683bc8e892ad2601af6430045058ccb7
shutooike <shutofootball@icloud.com>
2019-06-25 15:33:03 +0000

    Removed a bug again

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -16,7 +16,7 @@ class LinebotController < ApplicationController
 
 		signature = request.env['HTTP_X_LINE_SIGNATURE']
 		unless client.validate_signature(body, signature)
-			error 400 do 'Bad Request' end
+			head :bad_request
 		end
 
 		events = client.parse_events_from(body)
